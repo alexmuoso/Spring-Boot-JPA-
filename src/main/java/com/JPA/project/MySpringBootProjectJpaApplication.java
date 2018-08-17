f36adf067f99e7a1c7c20977b65ad04a3d0cfe01
Alejandro Muñoz Osorio <alejandro.mnuoz@4thsource.com>
2018-08-17 16:00:49 -0500

    first

---
+++ src/main/java/com/JPA/project/MySpringBootProjectJpaApplication.java
@@ -0,0 +1,12 @@
+package com.JPA.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootProjectJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootProjectJpaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/JPA/project/MySpringBootProjectJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.JPA.project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MySpringBootProjectJpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
